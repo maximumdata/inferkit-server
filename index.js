1fe2cafcca101131bd56698785ee2743cac0fa8f
Mike Dettmer <mike@mikedettmer.com>
2020-07-08 23:24:30 -0400

    adds root route and cors

--- index.js
+++ index.js
@@ -2,14 +2,20 @@ import express from 'express';
 import dotenv from 'dotenv';
 import bodyParser from 'body-parser';
 import router from './router';
+import cors from 'cors';
 
 dotenv.config();
 
 const app = express();
 const { env: { PORT } } = process;
 
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.send('gtfo 🖕');
+});
 
 app.use(router);
 
